5e943a99d57b6ee18b8817fbf639b93b1b972ed8
samier-fabien <samierfabien@gmail.com>
2021-12-10 12:52:04 +0100

    Mise en place des actualités

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -24,11 +24,11 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : "";
         $newsletters = $this->newsletterRepo->findLast(4);
         $actions = $this->actionRepo->findLast(4);
-        $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : "";
 
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/home.html.twig', [
             'newsletters' => $newsletters,
             'actions' => $actions,
             'userEmail' => $userEmail,
--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,31 +2,86 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\NewsletterRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class NewsletterController extends AbstractController
 {
-    /**
-     * @Route("/newsletter", name="newsletter")
-     */
-    public function index(): Response
+    private $newsletterRepo;
+
+    public function __construct(NewsletterRepository $newsletterRepo)
     {
-        return $this->render('newsletter/index.html.twig', [
-            'controller_name' => 'NewsletterController',
+        $this->newsletterRepo = $newsletterRepo;
+    }
+
+    /**
+     * @Route("/actualites", name="newsletters")
+     */
+    public function displayAll(): Response
+    {
+        $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : "";
+        $newsletters = $this->newsletterRepo->findAll();
+
+        return $this->render('newsletter/displayAll.html.twig', [
+            'userEmail' => $userEmail,
+            'newsletters' => $newsletters,
+        ]);
+    }
+
+    /**
+     * @Route("/actualite/{id}", name="newsletter")
+     */
+    public function display($id): Response
+    {
+        $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : "";
+        $newsletter = $this->newsletterRepo->findOneBy(["id" => $id]);
+        //dd($newsletter);
+
+        return $this->render('newsletter/display.html.twig', [
+            'userEmail' => $userEmail,
+            'newsletter' => $newsletter,
         ]);
     }
 
     /**
      * @IsGranted("ROLE_AGENT")
-     * @Route("/newsletter/agent/creation", name="newsletterCreation")
+     * @Route("/actualites/agent/creer", name="newsletterCreate")
      */
     public function create(): Response
     {
+        $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : "";
+
+        return $this->render('newsletter/create.html.twig', [
+            'userEmail' => $userEmail,
+        ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_AGENT")
+     * @Route("/actualites/agent/editer", name="newsletterUpdate")
+     */
+    public function update(): Response
+    {
+        $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : "";
+
+        return $this->render('newsletter/update.html.twig', [
+            'userEmail' => $userEmail,
+        ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_AGENT")
+     * @Route("/actualites/agent/supprimer", name="newsletterDelete")
+     */
+    public function delete(): Response
+    {
+        $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : "";
+
         return $this->render('newsletter/creation.html.twig', [
-            'controller_name' => 'NewsletterController',
+            'userEmail' => $userEmail,
         ]);
     }
 }
