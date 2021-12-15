300bab870cd8590ac4bedab39f488ddab9f8597d
samier-fabien <samierfabien@gmail.com>
2021-12-15 19:54:23 +0100

    Ajout dans la navbar du choix de la langue + Ajout d'un sbscriber qui se charge de  changer la locale + Ajout d'une route /langue/{locale} qui permet de changer de locale pour une contenue dans app.locales de services.yaml

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\ActionRepository;
 use App\Repository\NewsletterRepository;
+use Locale;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,10 +26,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Envoi de l'email de l'utilisateur dans template ou null s'il n'existe pas
         $userEmail = ($this->getUser()) ? $this->getUser()->getEmail() : null;
+        // Envoi des "x" dernieres newsletters dans le template
         $newsletters = $this->newsletterRepo->findLast(self::NUMBER_OF_NEWSLETTERS);
+        // Envoi des "x" dernieres actions dans le template
         $actions = $this->actionRepo->findLast(self::NUMBER_OF_ACTIONS);
 
         return $this->render('home/home.html.twig', [
@@ -35,5 +40,28 @@ class HomeController extends AbstractController
             'actions' => $actions,
             'userEmail' => $userEmail,
         ]);
+    }
+
+    /**
+     * @Route("/langue/{locale}", name="locale")
+     */
+    public function lang($locale, Request $request): Response
+    {
+        // Vérification que la locales est bien dans la liste des langues sinon retour accueil en langue française
+        $this->getParameter('app.locales');
+        if (!in_array($locale, $this->getParameter('app.locales'), true)) {
+            $request->getSession()->set('_locale', 'fr'); 
+            $request->getSession()->set('locale', 'fr');
+            return $this->redirect("/");
+        }
+        
+        // Stockage de la langue dans la session
+        $request->getSession()->set('_locale', $locale); 
+        $request->getSession()->set('locale', $locale);
+
+        // On revient à la page précédente si elle est dans le referer sinon vers l'accueil
+        $url = (!is_null($request->headers->get('referer'))) ? $request->headers->get('referer') : "/";
+        
+        return $this->redirect($url);
     }
 }
